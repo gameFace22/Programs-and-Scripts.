e5bd2f04a195813c91094d20d18c0820edcbfcb0
Nishaanth Gunasekaran <nishaanth.guna@gmail.com>
2014-11-25 01:23:41 +0530

    Update ip_range.sh

--- ip_range.sh
+++ ip_range.sh
@@ -3,13 +3,13 @@
 a=1
 while [ $a -ne 256 ]
  do
- ping -c 1 172.17.171.$a > /dev/null                        #/dev/null is a device file which discards all the data written to it.
+ ping -c 1 x.x.x.$a > /dev/null                        #/dev/null is a device file which discards all the data written to it.
  if [ $? -eq 0 ]                                              
  then
-   echo 172.17.171.$a IS UP >> ip_range_UP.txt
+   echo x.x.x.$a IS UP >> ip_range_UP.txt
    ((a++))
  else
-   echo 172.17.171.$a IS DOWN 
+   echo x.x.x.$a IS DOWN 
    ((a++))
  fi 
  done
