e9bb24148b25deb6f5db5ec5089b3132e447b9ad
Nishaanth Gunasekaran <nishaanth.guna@gmail.com>
2014-12-24 23:33:58 +0530

    Created a basic prototype of an IRC bot.

---
+++ hackbot.py
@@ -0,0 +1,44 @@
+
+import socket 
+
+
+server = "irc.freenode.net" 
+channel = "#hackademic-dev" 
+botnick =  "hackademic-bot" 
+
+
+def ping(): 
+  ircsock.send("PONG :pingis\n")  
+
+def sendmsg(chan , msg): 
+  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
+
+def joinchan(chan): 
+  ircsock.send("JOIN "+ chan +"\n")
+
+def hello(): 
+  ircsock.send("PRIVMSG "+ channel + "Hello" + "\n")
+
+def whois(): 
+  ircsock.send("PRIVMSG "+ channel +" Hackademic Bot :P\n")
+
+
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((server, 6667))
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is hackbot.\n") 
+ircsock.send("NICK "+ botnick +"\n") 
+joinchan(channel) 
+
+while 1: 
+  ircmsg = ircsock.recv(2048) 
+  ircmsg = ircmsg.strip('\n\r') 
+  print(ircmsg) 
+
+  if ircmsg.find("Issue") != -1: 
+    hello()
+
+  if ircmsg.find("PING :") != -1: 
+    ping()
+  
+  if ircmsg.find("Who is "+ botnick) != -1: 
+    whois()
